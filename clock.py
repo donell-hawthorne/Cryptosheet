a49283284cb22ded3270eee7091da0b0bc714da3
donell-hawthorne <36404381+donell-hawthorne@users.noreply.github.com>
2018-02-17 03:56:37 -0600

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,7 +3,5 @@ from apscheduler.scheduler.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=30)
-def timed_job():
-    print('This job is run every 30 minutes.')
-    
+
 sched.start()
